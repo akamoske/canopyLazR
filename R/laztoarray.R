6746408954d3af3084c5ec7d50e18cc14defff5c
akamoske <akamoske@gmail.com>
2018-08-09 14:47:45 -0400

    bugs

--- R/laztoarray.R
+++ R/laztoarray.R
@@ -31,13 +31,12 @@ laz.to.array <- function(laz.files.path, voxel.resolution, z.resolution) {
                                    col.names = c("x", "y", "z", "class"))
     
     #lets remove the points classified as noise
-    #laz.xyz <- laz.xyz.table[laz.xyz.table$class != "7",]
-    laz.xyz <- laz.xyz.table
-    
+    laz.xyz <- laz.xyz.table[laz.xyz.table$class != "7",]
+   
     #lets create some boundary information for the x,y,z columns
-    x.range.raw <- range(laz.xyz[,1])
-    y.range.raw <- range(laz.xyz[,2])
-    z.range.raw <- range(laz.xyz[,3])
+    x.range.raw <- range(laz.xyz[,1], na.rm = TRUE)
+    y.range.raw <- range(laz.xyz[,2], na.rm = TRUE)
+    z.range.raw <- range(laz.xyz[,3], na.rm = TRUE)
     
     #lets set how big we want each pixel to be
     x.y.grain <- voxel.resolution
