7118af554912eea933a067a8e51966116106585e
akamoske <akamoske@gmail.com>
2018-08-09 12:06:50 -0400

    Initial commit

---
+++ R/ladarraytorasterstack.R
@@ -0,0 +1,102 @@
+#' Convert a list of voxelized arrays into a single raster stack
+#' 
+#' This function reads in a list of voxelized arrays, converts each one to a raster stack, and then merges them all together
+#' to form a raster stack of the entire study area. This function also creats a lot of extra NA layers in the raster stack so that
+#' #this individual rasters can easily be merged together.
+#' 
+#' @param lad.array.list List of voxelized LAD arrays that was created with the machorn.lad function
+#' @param epsg.code EPSG code so that the rasters can be projected into the appropriate projection
+#' @return A raster stack of LAD estimates. Each raster is associated with a slice of the canopy. The Z resolution was determined
+#' in the laz.to.array function
+#' @export
+
+lad.array.to.raster.stack <- function(lad.array.list, epsg.code) {
+  
+  #lets create an empty list so that we can store the raster stacks in
+  lad.rstack.list <- list()
+  
+  #lets loop through the list of LAD arrays so that we can do some stuff to all of them
+  for (z in 1:length(lad.array.list)) {
+    
+    #lets pull out the first array in the list
+    testr <- lad.array.list[[z]]$rLAD
+    
+    #create an empty list so that we can store the new matrix in it
+    lad.mat <- list()
+    
+    #lets loop through all the rows of data in the array
+    for (i in 1:dim(testr)[1]) {
+      
+      #Lets create an empty matrix that corresponses with this row of data
+      m.lad <- matrix(data = NA, nrow = dim(testr)[2], ncol = dim(testr)[3])
+      
+      #lets loop through all the slices of data in the array
+      for (q in 1:dim(testr)[3]) {
+        
+        #lets assign a new column in the matrix as the row of data in that slice
+        #this will end up representing an indivdual column of of data at a given height in the canopy
+        #we will eventually end up with a matrix for each vertical interval of the canopy
+        m.lad[,q] <- testr[i,,q]
+      }
+      
+      #save the complete matrix to the list and go up to the next level in the canopy
+      lad.mat[[i]] <- m.lad
+    }
+    
+    #lets create a new list to store the rasters in
+    raw.lad.rasters <- list()
+    
+    #lets loop through the list of matrixes that we just created
+    for(f in 1:length(lad.mat)){
+      
+      #this is the projection code for your site, change it as needed.
+      crs.proj <- base::paste0("+init=epsg:", epsg.code)
+      
+      #now we can create a raster for each level of the canopy using the original x,y data from the laz data
+      lad.raster <- raster::raster(lad.mat[[f]],
+                                   xmn = laz.data[[z]]$x.bin[1],
+                                   xmx = laz.data[[z]]$x.bin[length(laz.data[[z]]$x.bin)],
+                                   ymn = laz.data[[z]]$y.bin[1],
+                                   ymx = laz.data[[z]]$y.bin[length(laz.data[[z]]$y.bin)],
+                                   crs = crs.proj)
+      
+      #we have to flip these rasters so that they are orientated the correct direction
+      #this is standard when converting from an array to a raster
+      lad.raster.flip <- flip(lad.raster, direction = "y")
+      
+      #lets save this completed raster to our list
+      raw.lad.rasters[[f]] <- lad.raster.flip
+    }
+    
+    #now that we have all the rasters for one tile, lets stack those all together
+    lad.rasters <- do.call(raster::stack, raw.lad.rasters)
+    
+    #since all the raster stacks will have a different number of layers depending on pulse return locations, we need to normalize them.
+    #the easiest way to do this was to assume that none of the tiles will have more than 100 layers in the raster stack. So we can just add 
+    #a certain number of layers to the stack so that they all will have 100 layers. This will allow us to merge them together.
+    new.layers.num <- 120 - nlayers(lad.rasters)
+    
+    #to do this, we need to make a copy of one of the layers so that is have the save extent and resolution values
+    new.raster <- lad.rasters[[1]]
+    
+    #lets assign all the cells in this raster as NA
+    new.raster[] <- NA
+    
+    #now lets add the needed number of rasters to the top of the stack so that they will all eventually be equal
+    for (k in 1:new.layers.num) {
+      lad.rasters <- addLayer(lad.rasters, new.raster)
+    }
+    
+    #lets save the finished raster stack to the list
+    lad.rstack.list[[z]] <- lad.rasters
+    
+    #print a statement to keep us on track
+    print(paste0("Raster number ", z, " is complete!"))
+  }
+  
+  #now that all the raster stacks and complete lets merge them together as one big happy raster
+  lad.ras <- base::do.call(merge, lad.rstack.list)
+  
+  #return the final raster
+  return(lad.ras)
+}
---
+++ R/laztoarray.R
@@ -0,0 +1,149 @@
+#' Convert a .laz or .las file to an array
+#' 
+#' This function reads in a list of .laz or .las files from a given file path and converts
+#' each one into a voxelized array based on the x,y,z resolutions you specify.
+#' 
+#' @param laz.files.path Path to the folder containing the .laz or .las files
+#' @param voxel.resolution The spatial resolution (x,y) you want the output voxel to be - this is a single 
+#' number where both sides of the cell will be the same
+#' @param z.resolution the vertical resolution of the voxel
+#' @return A list of voxelized arrays
+#' @export
+laz.to.array <- function(laz.files.path, voxel.resolution, z.resolution) {
+
+  #create an empty list
+  laz.list <- list()
+  
+  #list all the files in the path
+  laz.files.list <- list.files(laz.files.path, full.names = TRUE)
+  
+  #loop through the files so that we can use memory management techniques
+  for (i in 1:length(laz.files.list)) {
+    
+    #save the first file in the loop as a new variable
+    laz.file <- laz.files.list[i]
+    
+    #read in laz files
+    laz.data <- rlas::readlasdata(laz.file)
+    
+    #convert into a x,y,z, class table for easy reading and analysis
+    laz.xyz.table <- as.data.frame(c(laz.data[,"X"], laz.data[,"Y"], laz.data[,"Z"], laz.data[,"Classification"]), 
+                                   col.names = c("x", "y", "z", "class"))
+    
+    #lets remove the points classified as noise
+    laz.xyz <- laz.xyz.table[laz.xyz.table$class != "7",]
+    
+    #lets create some boundary information for the x,y,z columns
+    x.range.raw <- range(laz.xyz[,1], na.rm=T)
+    y.range.raw <- range(laz.xyz[,2], na.rm=T)
+    z.range.raw <- range(laz.xyz[,3], na.rm=T)
+    
+    #lets set how big we want each pixel to be, in this case 2x2x2
+    x.y.grain <- voxel.resolution
+    z.grain <- z.resolution
+    
+    #this is kind of repetitive, but makes sure that there are no rounding issues, thus any number
+    #with a decimal is rounded down (floor) or up (ceiling), this helps elimate edge weirdness and cell size issues
+    x.range <- c(floor(x.range.raw[1] / x.y.grain) * x.y.grain, ceiling(x.range.raw[2] / x.y.grain) * x.y.grain)
+    y.range <- c(floor(y.range.raw[1] / x.y.grain) * x.y.grain, ceiling(y.range.raw[2] / x.y.grain) * x.y.grain)
+    z.range <- c(floor(z.range.raw[1] / z.grain) * z.grain, ceiling(z.range.raw[2] / z.grain) * z.grain)
+    
+    #lets create the bins that will be used as cells later on
+    #this creates a 2x2 bin for the x and y axis and a 2x2x2 bin for the z axis
+    x.bin <- seq(x.range[1], x.range[2], x.y.grain)
+    y.bin <- seq(y.range[1], y.range[2], x.y.grain)
+    z.bin <- seq(z.range[1], z.range[2], z.grain)
+    
+    #find the idex number for each lidar pulse so that that pulse can be placed in the appropriate bin
+    #rounding to the middle point of the pixel, this assigns a value to each lidar point
+    x.cuts <- round((laz.xyz[,1] - x.bin[1] + x.y.grain / 2) / x.y.grain)
+    y.cuts <- round((laz.xyz[,2] - y.bin[1] + x.y.grain / 2) / x.y.grain)
+    z.cuts <- round((laz.xyz[,3] - z.bin[1] + z.grain / 2) / z.grain)    
+    
+    #here we turn the y value index numbers into a decimal place and then add that to the x value,
+    #this will give us a number of unique values equal to the number of pixels in the empty raster
+    #we can then order these, and only have to do indexing one time, rather than twice (x and y),
+    #thus improve performance time and computing efficiency
+    #integer is x index, float is y index
+    y.cuts.dec <- y.cuts / (length(y.bin))
+    x.y.cuts <- x.cuts + y.cuts.dec
+    
+    #lets create a data frame that has two columns, one with the x index values and another 
+    #with the y index values in decimal form
+    x.y.levels <- as.data.frame(expand.grid(1:(length(x.bin) - 1), (1:(length(y.bin) - 1)) / length(y.bin))) 
+    colnames(x.y.levels) <- c("x.level", "y.level")
+    
+    #lets reorder the x.y.levels data frame so that they are in numerical order
+    #first order = x lev, second order = y level
+    x.y.levels <- x.y.levels[order(x.y.levels[,"x.level"], x.y.levels[,"y.level"]),]
+    
+    #now that the variables are ordered, lets break the data frame apart and store the 
+    #data as a string of values
+    x.y.levels.char <- as.character(x.y.levels[,"x.level"] + x.y.levels[,"y.level"])
+    
+    #assign pixel values (index values) to the 250,000 pixels in the newly formed raster
+    x.y.cuts.factor <- factor(x.y.cuts, levels = x.y.levels.char)
+    
+    #lets create an empty matrix to store all this in, fill it with NA values for now so that it has shape
+    xyz.matrix <- as.data.frame(matrix(NA, nrow = length(x.y.levels.char), ncol = 4, dimnames = list(NULL, c("x","y","z", "class"))))
+    
+    #lets bind this empty matrix with the lidar data frame
+    xyz.table <- rbind(laz.xyz, xyz.matrix)
+    
+    #this determines the index number the z value of all lidar pulses, 
+    #so that each lidar pulse is assigned a vertical voxel to live in
+    z.index <- c(z.cuts, rep(NA, length(x.y.levels.char)))
+    
+    #this determines the index number for the x,y cell, so that each lidar pulse is assigned a raster cell to live in
+    x.y.index <- c(x.y.cuts.factor, as.factor(x.y.levels.char))
+    
+    #lets combine the lidar table, the x,y index values, and the z index values into a data frame
+    lidar.table <- data.frame(xyz.table, x.y.index = x.y.index, z.index = z.index)
+    
+    #lets create a function that populates the empty array that we will create later
+    #this function will (for each factor in the data frame) seperate the pre-classified ground points
+    #from the non-ground points, store the lowest ground value in the first row of the array, store the 
+    #highest non-ground point in the second row, and the number of lidar pulses in each z voxel being stored
+    #in each subsequent row with the lowest voxel being the 3rd row and the highest voxel being the last row
+    lidar.array.populator <- function(x){
+      ground.pts <- x[x[,4] == 2,]
+      as.numeric(c(
+        quantile(ground.pts[,3], prob = 0, na.rm = TRUE),
+        quantile(x[,3], prob = 1, na.rm = TRUE),
+        (table(c(x[,6], 1:length(z.bin) - 1)) - 1)
+      ))
+    }
+    
+    #lets see how many rows the empty array will have, this will be equal to the number of z voxels
+    #this will also test to make sure that there are no initial errors when applying the above function 
+    #to the whole data set
+    z.vox.test <- length(dlply(lidar.table[1:2,], "x.y.index", lidar.array.populator)[[1]])
+    
+    #generate the final array using the lidar table, the x.y.index values as the factor, and the lidar.array.populator
+    #as the function.  set the dimensions equal to the number of z voxels, the length of y.bin and the length of x.bin.
+    #we need to subtract 1 from each of these values, because there is a 0 y.bin and x.bin, due to data creation
+    #and edge effect mitigation -- there is no data stored here, but makes counting and rounding easier
+    lidar.array <- array(as.vector(unlist(dlply(lidar.table, "x.y.index", lidar.array.populator, .progress = "text"))), 
+                         dim = c(z.vox.test, (length(y.bin) - 1), (length(x.bin) - 1)))
+    
+    #lets return the array and the x.bin and y.bin so that we can have spatial information for later
+    return.data <- base::list("array" = lidar.array, "x.bin" = x.bin, "y.bin" = y.bin, "z.bin" = z.bin)
+    
+    #save the data to the list
+    laz.list[[i]] <- return.data
+    
+    #lets remove all the stuff we don't need anymore so that R doesn't have a melt down due to memory usage
+    gc()
+    remove(laz.data)
+    remove(laz.xyz.table)
+    remove(laz.xyz)
+    remove(lidar.table)
+    remove(lidar.array)
+    remove(return.data)
+    gc()
+  }
+  
+  #return the list
+  return(laz.list)
+  
+}
