4c456a660fa85effd6acb3478ce5ee2ac73ce59f
akamoske <akamoske@gmail.com>
2018-08-09 14:46:04 -0400

    debugging

--- R/laztoarray.R
+++ R/laztoarray.R
@@ -32,6 +32,7 @@ laz.to.array <- function(laz.files.path, voxel.resolution, z.resolution) {
     
     #lets remove the points classified as noise
     #laz.xyz <- laz.xyz.table[laz.xyz.table$class != "7",]
+    laz.xyz <- laz.xyz.table
     
     #lets create some boundary information for the x,y,z columns
     x.range.raw <- range(laz.xyz[,1])
